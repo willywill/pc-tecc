7eba9687f2bd50bdd02cdaaaca5338494c112eb5
ealucas <lucaseliasalves.a@gmail.com>
2021-05-05 12:28:45 -0300

    Adding Tailwind

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-blue-900">
           Edit
           {' '}
           <code>src/App.js</code>
